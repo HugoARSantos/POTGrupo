0d97c167ad87b7badddd291304d1a25e87447ef6
Ragmarok <hugo.alex.santos.93@gmail.com>
2017-12-11 01:03:20 +0000

    Global variables created

--- main.cpp
+++ main.cpp
@@ -15,11 +15,15 @@
 #include "Mundo.h"
 #include "Formiga.h"
 #include <iostream>
+#include "Comandos.h"
 using namespace std;
+extern int lim;                     //Limite do mundo, não pode ser alterado durante a simulação
+extern int nenergia;                //Energia inicial dos ninhos
+extern int menergia;                //Energia inicial das migalhas
+extern int percentagem;             //Posiçoes vazias ocupadas por migalhas ao inicio
+extern int transferencia;           //Percentagem de energia(vezes a inicial) que o ninho precisa para criar uma nova formiga
+extern int nmigalhas;               //Novas migalhas que aparecem a cada interação
 
-/*
- * 
- */
 int main() {
     Formiga a(1,2,10);
     cout << a.getAsString();
