038a38578ff09d062ba451b86af9d31dd34b42c3
Ragmarok <hugo.alex.santos.93@gmail.com>
2017-12-15 18:07:33 +0000

    Criação de logica

--- main.cpp
+++ main.cpp
@@ -13,9 +13,28 @@
 #include <string>
 using namespace std;
 
-
 int main() {
+    string op;
+    int i,j,k,l;
     Comandos consola();
+    i = NULL; j = NULL, k = NULL, l = NULL;
+    cout >> "Introduza um comando: ";
+    getline(cin,op, i);   
     
+    if (op == "defmundo")
+        if (i != NULL)
+            consola.defMundo(i);
+
+    if (op == "defen")
+        if (i != NULL)
+            consola.defEnergia(i);
+        
+    if (op == "defpc")
+        if (i != NULL)
+            consola.defPercentagemCriarFormiga(i);
+    
+    if (op == "defvt")
+        if (i != NULL)
+            consola.defValorTransferencia(i);
 }
 
